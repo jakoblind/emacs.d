f1dd1482d0c5179771b46d4819fd1cca06f0c162
Jakob Lind <karl.jakob.lind@gmail.com>
2014-09-02 15:14:05 +0200

    smarter move beginning of line

--- .emacs.d/keybindings.el
+++ .emacs.d/keybindings.el
@@ -47,13 +47,39 @@
 
 (global-set-key (kbd "M-S-<left>") (kbd "C-S-a"))
 (global-set-key (kbd "M-S-<right>") (kbd "C-S-e"))
-(global-set-key (kbd "M-<left>") (kbd "C-a"))
+(global-set-key (kbd "M-<left>") 'smarter-move-beginning-of-line)
 (global-set-key (kbd "M-<right>") (kbd "C-e"))
 (global-set-key (kbd "M-<up>") (kbd "M-<"))
 (global-set-key (kbd "M-<down>") (kbd "M->"))
 (global-set-key (kbd "M-<backspace>") 'delete-line)
 (global-set-key (kbd "S-<return>") (kbd "C-e <return>"))
 (global-set-key (kbd "M-<return>") (kbd "C-a <return> C-p"))
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line]
+		'smarter-move-beginning-of-line)
 
 (defun delete-line (&optional arg)
   (interactive "P")
