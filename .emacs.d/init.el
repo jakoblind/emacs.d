a9f6aab87177b506a3e6b4f3280de87377866d66
Jakob Lind <karl.jakob.lind@gmail.com>
2014-11-24 11:33:16 +0100

    enable menubar again, lol

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
 ;; Turnen off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+;;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 
 ;; No splash screen please ... jeez
