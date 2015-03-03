2d67e5698f10da55dc2a8b809c132b56818a46a2
Jakob Lind <karl.jakob.lind@gmail.com>
2015-03-03 12:46:20 +0100

    use js2-mode not minormode

--- lisp/javascript.el
+++ lisp/javascript.el
@@ -9,7 +9,7 @@
 (add-to-list 'auto-mode-alist '("\\.jsx$" . js-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
 
-(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-hook 'js-mode-hook 'js2-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
 ;; (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
