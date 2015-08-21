efc678493192cf1fcf232a232f71c90e923e5059
Jakob Lind <karl.jakob.lind@gmail.com>
2015-08-21 13:15:34 +0200

    keybinding stuff

--- lisp/keybindings-common.el
+++ lisp/keybindings-common.el
@@ -46,8 +46,8 @@ point reaches the beginning or end of the buffer, stop there."
 
 (global-set-key (kbd "C-s-<down>") (lambda () (interactive) (ignore-errors (next-line 5))))
 (global-set-key (kbd "C-s-<up>") (lambda () (interactive) (ignore-errors (previous-line 5))))
-(global-set-key (kbd "s-n") (lambda () (interactive) (ignore-errors (next-line 5))))
-(global-set-key (kbd "s-p") (lambda () (interactive) (ignore-errors (previous-line 5))))
+(global-set-key (kbd "M-n") (lambda () (interactive) (ignore-errors (next-line 5))))
+(global-set-key (kbd "M-p") (lambda () (interactive) (ignore-errors (previous-line 5))))
 
 (global-set-key (kbd "M-+") 'text-scale-increase)
 (global-set-key (kbd "M--") 'text-scale-decrease)
