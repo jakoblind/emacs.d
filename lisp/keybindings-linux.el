b795aa746ce5c4028c8656df48b4f520b12e1725
Jakob Lind <karl.jakob.lind@gmail.com>
2015-08-21 11:12:39 +0200

    added keybindings-linux

---
+++ lisp/keybindings-linux.el
@@ -0,0 +1,10 @@
+(global-set-key (kbd "M-o") 'projectile-find-file)
+(global-set-key (kbd "M-p") 'projectile-switch-project)
+
+(global-set-key (kbd "C-x b") 'ido-switch-buffer)
+
+(global-set-key (kbd "M-x") 'smex)
+
+;expand region here
+
+(provide 'keybindings-linux)
