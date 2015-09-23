d8e6f5aa07d0d1e2d1a01e473d566ff8efaecf6f
Jakob Lind <karl.jakob.lind@gmail.com>
2015-09-23 08:29:24 +0200

    Clean up JS file. to set JS2-Mode correctly

--- lisp/javascript.el
+++ lisp/javascript.el
@@ -6,15 +6,12 @@
             (lambda ()
               (push '("function" . ?λ) prettify-symbols-alist)))
 
-(add-to-list 'auto-mode-alist '("\\.jsx$" . js-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 
 (require 'js2-refactor)
 (add-hook 'js2-mode-hook #'js2-refactor-mode)
-;(autoload 'js2-mode "js2-mode" nil t)
-;(add-hook 'js-mode-hook 'js2-minor-mode)
 (add-hook 'js-mode-hook 'auto-insert-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
