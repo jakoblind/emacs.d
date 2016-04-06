876a0299dd14882e6677521b42054ccb9d89791a
Jakob Lind <karl.jakob.lind@gmail.com>
2016-04-06 12:09:15 +0200

    create appearance module

--- init.el
+++ init.el
@@ -53,19 +53,6 @@
 (when (eq system-type "cygwin")
   (set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 130))
 
-;increase font size, (and something else?)
-(set-face-attribute 'default nil :height 150)
-;(set-face-attribute 'default nil :height 120)
-
-(setq visible-bell nil);i want it to be t but doesnt work well on el capitan
-
-;; Highlight current line
-(global-hl-line-mode 1)
-(set-face-background hl-line-face "gray96")
-
-;show line number in footer
-(column-number-mode 1)
-
 (show-paren-mode t)
 
 (delete-selection-mode 1)
@@ -178,23 +165,7 @@
 (projectile-global-mode)
 (autopair-global-mode) ;; enable autopair in all buffers
 
-(load-theme 'noctilux t)
-;(disable-theme 'noctilux)
-                                        ;
-
-(global-visual-line-mode 1)
-
-(setq highlight-symbol-idle-delay 0)
-
-(setq speedbar-use-images nil
-      sr-speedbar-right-side nil
-      speedbar-show-unknown-files t
-      sr-speedbar-width 10
-      sr-speedbar-max-width 10
-      sr-speedbar-default-width 10
-      sr-speedbar-skip-other-window-p t)
-
-;(sr-speedbar-open)
+(require 'appearance)
 
 (require 'nodejs-repl-eval)
 
@@ -216,18 +187,6 @@
 (defalias 'ack-find-file 'ack-and-a-half-find-file)
 (defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
 (defalias 'ack-with-args 'ack-and-a-half-with-args)
-;;; Appearance
-(set-face-attribute 'mode-line
-                 nil
-                 :foreground "Black"
-                 :background "#aaccff"
-                 :box '(:line-width 1 :style released-button))
-
-(set-face-attribute 'mode-line-inactive
-                    nil
-                    :foreground "#aaccff"
-                    :background "Black"
-                    :box '(:line-width 1 :style released-button))
 
 
 (custom-set-variables
@@ -237,7 +196,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "46fd293ff6e2f6b74a5edf1063c32f2a758ec24a5f63d13b07a20255c074d399" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" "1a85b8ade3d7cf76897b338ff3b20409cb5a5fbed4e45c6f38c98eee7b025ad4" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "49eea2857afb24808915643b1b5bd093eefb35424c758f502e98a03d0d3df4b1" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" default)))
+    ("613a7c50dbea57860eae686d580f83867582ffdadd63f0f3ebe6a85455ab7706" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "46fd293ff6e2f6b74a5edf1063c32f2a758ec24a5f63d13b07a20255c074d399" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" "1a85b8ade3d7cf76897b338ff3b20409cb5a5fbed4e45c6f38c98eee7b025ad4" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "49eea2857afb24808915643b1b5bd093eefb35424c758f502e98a03d0d3df4b1" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" default)))
  '(sbt:program-name "/usr/local/bin/sbt")
  '(sgml-basic-offset 4))
 (custom-set-faces
---
+++ lisp/appearance.el
@@ -0,0 +1,75 @@
+;;; appearance.el ---                                -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Jakob Lind
+
+;; Author: Jakob Lind <karl.jakob.lind@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(load-theme 'noctilux t)
+;;(disable-theme 'noctilux)
+
+;; Increase font size, (and something else?)
+(set-face-attribute 'default nil :height 150)
+;;(set-face-attribute 'default nil :height 120)
+
+
+;; I want it to be t but doesnt work well on el capitan
+(setq visible-bell nil)
+
+
+;; Highlight current line
+(global-hl-line-mode 1)
+(set-face-background hl-line-face "gray30" )
+
+;show line number in footer
+(column-number-mode 1)
+
+;; Color of mode-line, the thing at the bottom of all windows
+(set-face-attribute 'mode-line
+                 nil
+                 :foreground "Black"
+                 :background "#aaccff"
+                 :box '(:line-width 1 :style released-button))
+
+(set-face-attribute 'mode-line-inactive
+                    nil
+                    :foreground "#aaccff"
+                    :background "Black"
+                    :box '(:line-width 1 :style released-button))
+
+(global-visual-line-mode 1)
+
+(setq highlight-symbol-idle-delay 0)
+
+(setq speedbar-use-images nil
+      sr-speedbar-right-side nil
+      speedbar-show-unknown-files t
+      sr-speedbar-width 10
+      sr-speedbar-max-width 10
+      sr-speedbar-default-width 10
+      sr-speedbar-skip-other-window-p t)
+
+;(sr-speedbar-open)
+
+
+(provide 'appearance)
+;;; appearance.el ends here
