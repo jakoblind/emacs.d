62c326a82b45bf8441a1c9e131a4a8a9c5243be8
Jakob Lind <karl.jakob.lind@gmail.com>
2016-09-22 09:02:38 +0200

    some stuff added by package.el

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 ;; Turnen off mouse interface early in startup to avoid momentary display
 ;;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(package-initialize)
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 
 ;; No splash screen please ... jeez
