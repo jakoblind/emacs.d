9b58ee493b73d7f067db46709e919121fbaea6d7
Jakob Lind <karl.jakob.lind@gmail.com>
2016-10-22 08:20:07 +0200

    use eslint from current project

--- lisp/javascript.el
+++ lisp/javascript.el
@@ -43,6 +43,18 @@
 
 (require 'flycheck)
 
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
 (setq-default flycheck-disabled-checkers
   (append flycheck-disabled-checkers
           '(javascript-jshint)))
