4582b6bf63e5b7190efc4ac3d4a8b92494d1522a
Jakob Lind <karl.jakob.lind@gmail.com>
2016-10-28 16:49:35 +0200

    add js2r-es6 package

--- lisp/javascript.el
+++ lisp/javascript.el
@@ -35,6 +35,8 @@
 ;;      (tern-ac-setup)))
 
 (require 'js2-refactor)
+(add-to-list 'load-path "~/.emacs.d/js2r-es6")
+(require 'js2r-es6)
 (add-hook 'js-mode-hook #'js2-refactor-mode)
 (add-hook 'js-mode-hook #'flycheck-mode)
 (add-hook 'js-mode-hook 'auto-insert-mode)
